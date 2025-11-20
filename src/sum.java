463bd972b90959681091142a2f4f9b5f10f27156
rww1218 <230814867@qq.com>
2025-11-20 16:39:03 +0800

    sum.java

--- src/sum.java
+++ src/sum.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner("计科六班-任雯-2405010624");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println(a+b);
+    }
 }
