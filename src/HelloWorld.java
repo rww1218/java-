695a98aebbcc29216e0175da587e8171c8bba242
任雯 <230814867@qq.com>
2025-11-20 15:52:39 +0800

    1. 输出 Hello World。

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("计科六班-任雯-2405010624");
+        System.out.println("Hello, World!");
+    }
 }
+
