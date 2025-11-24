d5f829a3843851798aa92bfeb69ee085fe6323a4
ofb777 <ofb241223@gmail.com>
2025-11-24 20:15:06 +0800

    第三题

---
+++ Circle1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Circle1 {
+    public static void main(String[] args) {
+        System.out.println("计科六班-任雯-2405010624");
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        System.out.println(Math.PI*r*r);
+    }
+}
