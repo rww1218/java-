ecba26b1e1e35bd3844b09f85820f27476769da7
ofb777 <ofb241223@gmail.com>
2025-11-24 20:14:22 +0800

    第二题

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("计科六班-任雯-2405010624");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
